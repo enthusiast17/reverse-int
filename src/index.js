efdd5e65dcc0f9f71ff57cfba08bb6da365f24bf
enthusiast17 <ulan17nurym@gmail.com>
2020-10-06 00:16:26 +0500

    feat: add reverse int solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    // let res = 0
+    // if (n < 0) n = -n
+    // while (n > 0) {
+    //     res = res * 10 + n % 10
+    //     n = parseInt(n / 10)
+    // }
+    // return res
+    return parseInt([...String(n)].reverse().join(''))
 }
+
